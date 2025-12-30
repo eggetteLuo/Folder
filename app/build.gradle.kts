185511978add47302dda714e376ff5a73d648cd2
xiaoluo <3428237080@qq.com>
2025-12-30 17:33:34 +0800

    feat: init - 实现文件管理器基本功能 - 添加文件和文件夹列表显示 - 实现目录导航和返回上一级功能 - 增加文件访问权限请求逻辑 - 引入相关依赖库 (ViewModel, PermissionX, Material Icons)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +58,13 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // 权限获取
+    implementation(libs.permissionx)
+
+    // 图标
+    implementation(libs.androidx.compose.material.icons.extended)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 }
---
+++ app/src/main/java/com/eggetteluo/folder/model/FileItem.kt
@@ -0,0 +1,9 @@
+package com.eggetteluo.folder.model
+
+data class FileItem(
+    val name: String,
+    val path: String,
+    val isDirectory: Boolean,
+    val size: Long,
+    val lastModified: Long
+)
