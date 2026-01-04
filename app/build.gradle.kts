1f3a7583a83919728f807faee914d2e0b6e77e04
xiwangly2 <xiwangly2@gmail.com>
2026-01-04 14:17:52 +0800

    chore: update project configuration for Java 25 and Gradle 9.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
     }
     buildFeatures {
         compose = true
